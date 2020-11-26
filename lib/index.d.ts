7c78792d1ac4e1c45d3a69b98913e784d697adaa
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-11-26 07:44:44 -0600

    Canceler

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,21 @@
+/// <reference types="node" />
+import { EventEmitter } from 'events';
+import { PassThrough } from 'stream';
+declare class Canceler extends PassThrough implements Promise<void> {
+    [Symbol.toStringTag]: string;
+    promise: Promise<void>;
+    canceled: boolean;
+    constructor(emitter: EventEmitter);
+    then<TResult1 = void, TResult2 = never>(onfulfilled?: ((value: void) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>;
+    catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): Promise<TResult>;
+    finally(onfinally?: (() => void) | undefined | null): Promise<void>;
+}
+declare type BuilderReturns = string | Buffer;
+declare type Builder = BuilderReturns | ((canceler?: Canceler) => void | BuilderReturns | Promise<BuilderReturns> | ReadableStream);
 declare class FileOutput {
+    outputPath: string;
+    emitter: EventEmitter;
+    constructor(outputPath: string);
+    update(builder: Builder): Promise<void>;
 }
 export default FileOutput;
--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,50 @@
-class FileOutput {
+import { EventEmitter, once } from 'events'
+import { PassThrough } from 'stream'
+import { writeFile } from 'fs/promises'
 
+class Canceler extends PassThrough implements Promise<void> {
+    [Symbol.toStringTag] = 'Manager'
+    promise: Promise<void>
+    canceled: boolean = false
+
+    constructor(emitter: EventEmitter) {
+        super()
+        this.promise = once(emitter, 'cancel').then(() => {
+            this.canceled = true
+        })
+    }
+
+    then<TResult1 = void, TResult2 = never>(onfulfilled?: ((value: void) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null) {
+        return this.promise.then<TResult1, TResult2>(onfulfilled, onrejected)
+    }
+    catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null) {
+        return this.promise.then<TResult>(onrejected)
+    }
+    finally(onfinally?: (() => void) | undefined | null) {
+        return this.promise.finally(onfinally)
+    }
+}
+type BuilderReturns = string | Buffer
+type Builder = BuilderReturns | ((canceler?: Canceler) => void | BuilderReturns | Promise<BuilderReturns> | ReadableStream)
+
+class FileOutput {
+    outputPath: string
+    emitter: EventEmitter
+
+    constructor(outputPath: string) {
+        this.outputPath = outputPath
+        this.emitter = new EventEmitter()
+    }
+
+    async update(builder: Builder) {
+        if (typeof builder === 'function') {
+            const canceler: Canceler = new Canceler(this.emitter)
+            builder(canceler)
+        }
+        else {
+            await writeFile(this.outputPath, builder)
+        }
+    }
 }
 
 export default FileOutput
